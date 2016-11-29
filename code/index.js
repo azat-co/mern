0bbcd04429cfa32966c7d8d799c73beaf63d2514
azat-co <hi@azat.co>
2016-11-29 14:13:09 -0600

    add changes :mans_shoe: :trident:

--- code/index.js
+++ code/index.js
@@ -2,13 +2,14 @@ const express = require('express'),
   mongodb = require('mongodb'),
   app = express(),
   bodyParser = require('body-parser'),
-  url = 'mongodb://localhost:27017/board'
+  url = process.env.MONGODB_URI || 'mongodb://localhost:27017/board'
 
 
 mongodb.MongoClient.connect(url, function(err, db) {
   if (err) {
     console.error(err)
     process.exit(1)
+    
   }
 
   app.use(bodyParser.json())
